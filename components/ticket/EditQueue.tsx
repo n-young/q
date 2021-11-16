59c13fd8a800d8e2068ec1ea810371f5a5854b87
Nick Young <youngnicholas228@gmail.com>
2021-11-16 10:38:02 -0500

    edit queue

--- components/ticket/EditQueue.tsx
+++ components/ticket/EditQueue.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Modal from "react-modal";
 import { doc } from "firebase/firestore";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import { firestore } from "../../util/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../util/firebase";
-import { modalStyle } from "../../util/constants";
+import { modalStyle, nextNHours } from "../../util/constants";
+import { setQueue } from "../../util/db";
+import styles from "./Ticket.module.css";
 
 interface EditQueueModalProps {
     isOpen: boolean;
@@ -17,8 +19,24 @@ function EditQueueModal({ isOpen, closeModal, qid }: EditQueueModalProps) {
     const [queue, loading, error] = useDocumentData(
         doc(firestore, "queues", qid || "dummy")
     );
+    const times = nextNHours(12);
 
-    return (
+    const setNewQueue = (newQueue: any) => {
+        const toset = {
+            ...queue,
+            ...newQueue,
+        };
+        setQueue(
+            toset.id,
+            toset.course,
+            toset.title,
+            toset.location,
+            toset.zoomLink,
+            toset.endTime.toDate()
+        );
+    };
+
+    return !loading && queue ? (
         <Modal
             isOpen={isOpen}
             onRequestClose={closeModal}
@@ -26,7 +44,37 @@ function EditQueueModal({ isOpen, closeModal, qid }: EditQueueModalProps) {
             contentLabel="Edit Queue"
         >
             <h2>Edit Queue</h2>
+            <form className={styles.form}>
+                <label>Title:</label>
+                <input
+                    value={queue.title}
+                    onChange={(e) => setNewQueue({ title: e.target.value })}
+                />
+                <label>Location:</label>
+                <input
+                    value={queue.zoomLink}
+                    onChange={(e) => setNewQueue({ zoomLink: e.target.value })}
+                />
+                <label>End time:</label>
+                <select
+                    onChange={(x) =>
+                        setNewQueue({
+                            endTime: times[parseInt(x.target.value, 10)],
+                        })
+                    }
+                    required
+                >
+                    <option value={-1} disabled selected></option>
+                    {times.map((y, i) => (
+                        <option value={i} key={i}>
+                            {y.format("h:mm A")}
+                        </option>
+                    ))}
+                </select>
+            </form>
         </Modal>
+    ) : (
+        <></>
     );
 }
 
